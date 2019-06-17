4a35ac2b52f681130bdead2c9981e694f374474e
aiLinXu <2693422196@qq.com>
2019-06-17 16:04:45 +0800

    提交git

---
+++ src/main/java/cn/itcast/git/demo/GitTest.java
@@ -0,0 +1,13 @@
+package cn.itcast.git.demo;
+
+/**
+ * @author xualin
+ * @version v1.0
+ * @date 2019/4/2 11:45
+ * @description
+ **/
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
